7a8fe72f850e9b1d8d6c09764a7972953d593786
Prasanna Udara Jayawardana <udara.fossdev@gmail.com>
2018-06-10 08:02:29 +0530

    scafolded frontend with grunt added

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vaultDragonFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vaultDragonFeApp
+ */
+angular.module('vaultDragonFeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vaultDragonFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vaultDragonFeApp
+ */
+angular.module('vaultDragonFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
